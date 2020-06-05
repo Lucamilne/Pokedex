78efde53ea78e2f75c84bdd8a06deeef333d28f9
Luca Milne <lucamilne@googlemail.com>
2020-06-05 13:32:17 +0100

    Updates

---
+++ precache-manifest.7b51c31e23b1b9257121488029b88c96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ccc46ab14f6fde0df38b428ae34a33",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "f3132ce4c43d54cb822d",
+    "url": "/Pokedex/static/css/main.123a77a2.chunk.css"
+  },
+  {
+    "revision": "d6c1fb9ef3094e415649",
+    "url": "/Pokedex/static/js/2.b6fec383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex/static/js/2.b6fec383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3132ce4c43d54cb822d",
+    "url": "/Pokedex/static/js/main.abebd3b4.chunk.js"
+  },
+  {
+    "revision": "c129e04ae51e70ff4d15",
+    "url": "/Pokedex/static/js/runtime-main.fda359d6.js"
+  }
+]);
