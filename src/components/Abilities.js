c05445f8347e32cf3094ccee27bf5cb42963758d
lucamilne <lucamilne@googlemail.com>
2020-11-02 23:22:44 +0000

    conditional added to abilities.js

--- src/components/Abilities.js
+++ src/components/Abilities.js
@@ -2,14 +2,18 @@ import "./abilities.css"
 import React from "react";
 
 const Abilities = (props) => {
-    const abilities = props.abilities.map(ability => {
-        return <div key={ability.name} className="nes-container is-rounded ability">
-            <h3 className="label">{ability.name}</h3>
-            <p>{ability.effect}</p>
-        </div>
-    })
-
-    return abilities;
+    return (
+        <>
+            {props.abilities &&
+                props.abilities.map(ability => (
+                    <div key={ability.name} className="nes-container is-rounded ability">
+                        <h3 className="label">{ability.name}</h3>
+                        <p>{ability.effect}</p>
+                    </div>
+                ))
+            }
+        </>
+    )
 }
 
 export default Abilities; 
